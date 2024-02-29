55d2765bc312deca0a43001052f93b686799ad5f
KoreanCow <vue-cli@kakao.com>
2024-02-29 19:04:25 +0900

    Chore: Update import globals.css and add background color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from 'next/font/google';
-import "./globals.css";
+import './globals.css'
 import styles from '@/app/(beforelogin)/_component/main.module.css';
 
 const inter = Inter({ subsets: ['latin'] });
